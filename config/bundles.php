f243c2213f99703131e760254755cd91a4340401
Dave Redfern <david.redfern@madagasgar.com>
2019-11-22 15:46:11 -0500

    Update to Symfony 4.4 base
    Update to DoctrineBundle 2.0
    Update to dama/doctrine-test-bundle
    Update doctrine config and cache services

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Adamsafr\FormRequestBundle\AdamsafrFormRequestBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true, 'docker' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
